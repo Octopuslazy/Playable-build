1b4622576fd3dfb153610842e4e25c615b987cd9
Octopuslazy <long.le@leapstud.io>
2026-04-07 17:35:20 +0700

    test build 2

--- dist/index.b35b4.js
+++ dist/index.b35b4.js
@@ -1,4 +1,4 @@
-System.register(["./application.e1f3f.js"], function (_export, _context) {
+System.register(["./application.fc3cb.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
