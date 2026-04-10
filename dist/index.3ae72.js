446effe127610458d8d5b347598d8d9e8e38ee34
Octopuslazy <long.le@leapstud.io>
2026-04-10 15:43:28 +0700

    upd build 2

--- dist/index.3ae72.js
+++ dist/index.3ae72.js
@@ -1,4 +1,4 @@
-System.register(["./application.6944c.js"], function (_export, _context) {
+System.register(["./application.86c07.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
