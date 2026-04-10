d2ccc9cf726cf9b9fba8aebab053198dfbb5c434
Octopuslazy <long.le@leapstud.io>
2026-04-10 16:46:11 +0700

    upd ver

--- dist/index.10034.js
+++ dist/index.10034.js
@@ -1,4 +1,4 @@
-System.register(["./application.bd63a.js"], function (_export, _context) {
+System.register(["./application.b212a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
