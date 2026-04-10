c345d3bd1f85566c2ec201222410c2a7b6f26776
Octopuslazy <long.le@leapstud.io>
2026-04-10 15:32:51 +0700

    upd build

--- dist/index.e606c.js
+++ dist/index.e606c.js
@@ -1,4 +1,4 @@
-System.register(["./application.b1ced.js"], function (_export, _context) {
+System.register(["./application.6944c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
